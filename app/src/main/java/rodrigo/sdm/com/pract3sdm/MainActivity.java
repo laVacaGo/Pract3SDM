a188e5a527af4b9a5297db58bd6cf078645b9b33
rodirgal <gacelathompson64@gmail.com>
2017-03-06 04:40:54 +0100

    Interfaz principal hecha(falta credits)

--- app/src/main/java/rodrigo/sdm/com/pract3sdm/MainActivity.java
+++ app/src/main/java/rodrigo/sdm/com/pract3sdm/MainActivity.java
@@ -1,7 +1,10 @@
 package rodrigo.sdm.com.pract3sdm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+    public void lanzarIntent(View v){
+        Intent intent;
+        switch (v.getId()) {
+            case R.id.buttonPlay:
+                intent= new Intent(this,PlayActivity.class);
+                if(intent.resolveActivity(getPackageManager()) != null)startActivity(intent);
+                break;
+            case R.id.buttonScores:
+                intent= new Intent(this,ScoreActivity.class);
+                if(intent.resolveActivity(getPackageManager()) != null)startActivity(intent);
+                break;
+            case R.id.buttonSettings:
+                intent= new Intent(this,SettingsActivity.class);
+                if(intent.resolveActivity(getPackageManager()) != null)startActivity(intent);
+                break;
+        }
+
     }
 }
